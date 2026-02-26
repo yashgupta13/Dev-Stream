d0f3153df7beb914841606f917a213e1b50a0940
Yash gupta <76024829+yashgupta13@users.noreply.github.com>
2026-02-27 00:34:33 +0530

    Delete backend directory

--- backend/api/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getStreamToken } from "../controllers/chatController.js";
-import { protectRoute } from "../middleware/protectRoute.js";
-
-const router = express.Router();
-
-router.get("/token", protectRoute, getStreamToken);
-
-export default router;
--- backend/api/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { protectRoute } from "../middleware/protectRoute.js";
-import { syncUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/sync", protectRoute, syncUser);
-
-export default router;
